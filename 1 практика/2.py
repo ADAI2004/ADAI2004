456a48f53b60777c37aaba0a0048badd577ead76
ADAI2004 <102576991+ADAI2004@users.noreply.github.com>
2022-03-29 13:56:38 +0600

    Add files via upload

---
+++ 1 практика/2.py
@@ -0,0 +1,6 @@
+name = input("Your name? ")
+age = int(input("Your age? "))
+home = input("where do you live? ")
+print("Your name:",name)
+print('Your age: ' + str(age))
+print('where do you live:',home)
---
+++ 1 практика/4.py
@@ -0,0 +1,8 @@
+x=5
+y=6
+print("x= ",x)
+print("y= ",y)
+z=x+yprint("z= ",z)
+z=x-yprint("z= ",z)
+z=x*yprint("z= ",z)
+z=x/yprint("z= ",z)
